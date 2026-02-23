dd2aa7058ebf83adf522d445bb4e39803fb3fd0e
rasp602 <rasp602@gmail.com>
2026-02-23 13:39:00 -0400

    otra prueba

--- tarjetas/modelo/tarjetas.php
+++ tarjetas/modelo/tarjetas.php
@@ -580,7 +580,7 @@ public function ListarTarjetasPorFecha($fecha)
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 	
-public function ExisteTarjeta($fechaSalida, $horaTarjeta,$idVariante)
+public function ExisteTarjeta1($fechaSalida, $horaTarjeta,$idVariante)
 {
     $sql = "SELECT 1
             FROM tarjeta
