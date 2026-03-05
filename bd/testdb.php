3b99893c5a3a36c92733d0b8aa1f2567f9f60180
rasp602 <rasp602@gmail.com>
2026-03-05 17:16:10 -0400

    act

--- bd/testdb.php
+++ bd/testdb.php
@@ -4,7 +4,7 @@ try {
 
     $pdo = new PDO(
         "mysql:host=193.203.175.238;port=3306;dbname=BASE_DATOS;charset=utf8",
-        "USUARIOu854084565_Rodri24",
+        "u854084565_Rodri24",
         "Rodrigo2410$"
     );
 
