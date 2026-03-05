1d8b8a6ca0553930ad20a405077c80540849bfc9
rasp602 <rasp602@gmail.com>
2026-03-05 18:14:03 -0400

    vamos a ver

--- bd/databaseServer.php
+++ bd/databaseServer.php
@@ -2,10 +2,25 @@
 class DatabaseLocal
 {
     public static function ConectarLocal()
+
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=trackgpszulia;charset=utf8', 'rasp602', 'Rodrigo2410$');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
-        return $pdo;
+        try {
+
+            $host = "srv1056.hstgr.io";
+            $dbname = "u854084565_trackgpsz1";
+            $user = "u854084565_Rodri24";
+            $pass = "Rodrigo2410$";
+
+            $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $pdo;
+
+        } catch (PDOException $e) {
+
+            die("Error de conexión: " . $e->getMessage());
+
+        }
     }
 }
 /*
--- bd/db.php
+++ bd/db.php
@@ -1,7 +1,13 @@
 <?php
 // simple conexion a la base de datos
 function connect(){
-	return new mysqli("srv1056.hstgr.io","u854084565_Rodri24","Rodrigo2410$","u854084565_trackgpsz1");
-}
 
+    $conn = new mysqli("srv1056.hstgr.io","u854084565_Rodri24","Rodrigo2410$","u854084565_trackgpsz1",3306);
+
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
 ?>
