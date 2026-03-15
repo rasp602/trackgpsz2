d3031e7ecbe83f6308d904e04718565957e7bee4
rasp602 <rasp602@gmail.com>
2026-03-15 18:08:40 -0300

    header_roles

--- includes/header_roles.php
+++ includes/header_roles.php
@@ -58,8 +58,7 @@
         <script src="js/header.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://kit.fontawesome.com/1d0ff78093.js" crossorigin="anonymous"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
-        <script src="/assets/js/IFrame.js"></script>
+      
 <div class="wrapper">
 
 
